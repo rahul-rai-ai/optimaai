1b60a97294bcd08911dd133635bbba59cdd5e424
rahul-rai-ai <rahulrai@outlook.ie>
2025-05-27 14:08:10 +0200

    Update index.ts

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,17 +1,12 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-
 import translationEN from './locales/en.json';
 import translationDE from './locales/de.json';
 
 const resources = {
-  en: {
-    translation: translationEN
-  },
-  de: {
-    translation: translationDE
-  }
+  en: { translation: translationEN },
+  de: { translation: translationDE },
 };
 
 i18n
@@ -20,9 +15,14 @@ i18n
   .init({
     resources,
     fallbackLng: 'en',
+    detection: {
+      order: ['cookie', 'navigator', 'htmlTag', 'path', 'subdomain'],
+      caches: ['cookie'],
+      cookieMinutes: 525600, // 1 year
+    },
     interpolation: {
-      escapeValue: false
-    }
+      escapeValue: false,
+    },
   });
 
 export default i18n;
